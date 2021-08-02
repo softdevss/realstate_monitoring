d954e7fe41f597b749a2e22dce04455941f7f56e
Arlan Camacho <arlancamacho19@gmail.com>
2021-08-02 14:47:01 +0800

    new design for admin login

--- adminlogin.php
+++ adminlogin.php
@@ -1,55 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="css/admin.css">
     <title></title>
+    <style>
+        body {background-color: #FF92A5;}
+    </style>
 </head>
-<style type="text/css">
-    body 
-    {
-        background-color: red;
-    }
-    #home
-    {
-        float: right;
-    }
-</style>
 <body>
-    <div id ="home">
-        <a href="index.php"><font color="white"><button>HOME</button></font></a>
-    </div>
-<center><section id="adminlogin">
+<section id="adminlogin">
         <div class="container">
             <div class="box last">
                 <div class="row">
-                    <div class="col-sm-6">
-                        <h1>Authentified Administrator</h1>
+                    <div class="col-lg-6 m-auto">
+                        <div class="card-title bg-black text-black">
+                      
                         <div class="status alert alert-success" style="display: none"></div>
                         <form method="post" action="login.php">
                             <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                         <label>Admin Username:</label>
-                                        <input type="text" class="form-control" required="required" name="username" placeholder="Admin Username">
+                                <div class="login-page">    
+                                <div class="form">
+                                    <div class="register-form">
+                                        <img src="css/images/logo_login.jpg" width="150" height="150"> <br><br>
+                                        <input type="text" class="form-control" required="required" name="username" placeholder="Admin Username:">
+                                        <input type="password" class="form-control" required="required" name="password" placeholder="Admin Password:">
+                                        <button type="submit" name="login">SIGN IN</button> <br><br>
+                                        <button onclick="window.location.href='index.php'">Home</button>
+                                        </form>
+                                    </div>
+                                    </div>
                                     </div>
                                 </div>
-                                 <br />
-                                <br />
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                         <label>Admin Password:</label>
-                                        <input type="password" class="form-control" required="required" name="password" placeholder="Admin Password">
-                                    </div>
-                                    <br />
-                                    <br />
-                                    <div class="form-group">
-                                     <center>   <button type="submit" name="login" class="btn btn-primary btn-lg">LOGIN</button></center>
-                                    </div>
+                        
                                 </div>
                                 
                             </div>
                   
                         </form>
                     </div><!--/.col-sm-6-->
+                    </div>
                     
                         
                         
@@ -58,8 +47,6 @@
                 </div><!--/.row-->
             </div><!--/.box-->
         </div><!--/.container-->
-    </section><!--/#contact-->
-    </center>
+    </section><!--/#contact-->  
 </body>
-</html>>
-
+</html>
